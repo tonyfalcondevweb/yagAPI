52c6a373cc1a721a52b7c094a2ac7fee8e4f476a
Tony Falcon <tonyfalcondevweb@gmail.com>
2023-03-20 16:48:01 +0100

    essaie origin

---
+++ src/main/java/root/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package root;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		
+		registry.addMapping("/**").allowedOrigins("https://way-one.vercel.app/");	
+		
+	}
+
+}
